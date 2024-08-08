fc644e49f98de174405db6840ab64c3a47de5114
Angelo Michele Pizzi <154350496+passionext@users.noreply.github.com>
2024-08-08 16:06:14 +0200

    Update script.py
    v1.0

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import socket
 from urllib.request import urlopen
 import re
 import netifaces
@@ -7,9 +8,11 @@ from scapy.sendrecv import srp
 
 def get_public_ip():
     """Obtain host's Public IP"""
+    # Urllib.request is a Python module for fetching URLs
     raw_ip = str(urlopen('http://checkip.dyndns.com/').read())
+    # Create the IP address's pattern and then seek for it in the fetched string
     pattern = r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.\d{1,3}"
-    return re.findall(pattern, raw_ip)
+    return re.findall(pattern, raw_ip)[0]
 
 
 def get_private_ip():
@@ -19,65 +22,86 @@ def get_private_ip():
     # List of interface identifiers for your machine.
     net_int = netifaces.interfaces()
     # Removing the loopback interface from the list.
-    net_int.remove("lo")
+    # net_int.remove("lo") --> REMOVED: Just select net_int[1] that is always the interfaces with IPv4 private address.
     # If at least one interface exist, and so the list net_int is not empty, the method enters this statement
     if net_int:
-        for element in net_int:
-            # Check the address of a particular interface.
-            net_value = netifaces.ifaddresses(element)
+        # Check the address of a particular interface.
+        net_value = netifaces.ifaddresses(net_int[1])
         # 2 is AF_INET (normal Internet addresses), 0 is the position of the last dictionary in the list, addr is the
         # key of the last dictionary, that contains the IP Address
-        return net_value[2][0]["addr"], net_value[2][0]["netmask"]
+    return net_value[2][0]["addr"], net_value[2][0]["netmask"]
 
 
-print(get_public_ip())
-addr, mask = get_private_ip()
-
-
-def convert_mask_dec_to_bin():
-    a = list(map(int, mask.split(".", 4)))
+def convert_mask_dec_to_bin(mask):
+    # Split and convert every octet in an int value. Then, add every octet (4 in total) in a list.
+    split_mask = list(map(int, mask.split(".", 4)))
+    # Initialize a list than is going to be fill with four binary octet
     mask_bit = []
-    print(a)
-    while a:
-        cifra = a.pop(0)
-        if cifra != 0:
-            mask_bit.append(bin(cifra).removeprefix("0b"))
+    # If the mask exist and is not empty
+    while split_mask:
+        value = split_mask.pop(0)
+        if value != 0:
+            # The bin() method converts a specified integer number to its binary representation and returns it.
+            # It is needed to remove the prefix that indicates that what bin return is a string and not an int!
+            fragment = bin(value).removeprefix("0b")
+            if len(fragment) < 8:
+                # Every octet must have eight element. For smaller number than 128, a varying quantity of zero has to be
+                # added.
+                fragment = (8 - len(fragment)) * "0" + fragment
+                mask_bit.append(fragment)
+            else:
+                # If the number is higher than 127, just add the number to the list.
+                mask_bit.append(fragment)
         else:
             # str.zfill(width) @ Return the numeric string left filled with zeros in a string of length width. A sign
-            # prefix is handled correctly. The original string is returned if width is less than or equal to len(s).
+            # prefix is handled correctly. The original string is returned if width is less than or equal to len(s)
             mask_bit.append("".zfill(8))
-        return mask_bit
+    # Define a delimiter
+    delimiter = ""
+    # The .join() method can concatenate list elements into a single string based on a delimiter
+    # THus, count is used to enumerate the number of zeros that the mask has got.
+    no_zeros = delimiter.join(mask_bit).count("0")
+    no_hosts = abs(2 ** no_zeros - 2)
+    mask_dec = str(32 - no_zeros)
+    return no_hosts, mask_dec
 
 
-def calculate_network(addr, mask):
-    networkl = []
-    a = list(map(int, mask.split(".", 4)))
-    addrlist = list(map(int, addr.split(".", 4)))
-    print(addrlist)
-    while addrlist:
-        no = addrlist.pop(0) & a.pop(0)
-        networkl.append(no)
-    print(networkl)
+def calculate_network(address, net_mask):
+    """Calculate the network address to which the host belongs"""
+    # Initialize the list that is going to contain the four octet
+    net_addr = []
+    network_addr = ""
+    split_mask = list(map(int, net_mask.split(".", 4)))
+    split_addr = list(map(int, address.split(".", 4)))
+    while split_addr:
+        # The result of the bitwise AND operation of IP address and the subnet mask is the network prefix
+        value = split_mask.pop(0) & split_addr.pop(0)
+        net_addr.append(str(value))
+        delimiter = "."
+        network_addr = delimiter.join(net_addr)
+    return network_addr
 
 
-##aggiungere il numero di host possibili in base alla maschera
+def hostname(addr):
+    """Get the hostname through the use of getnameinfo function in built-in socket module"""
+    # Need to provide a host+port tuple, but you can provide 0 for the port, and you'll get the hostname back.
+    name = socket.getnameinfo((addr, 0), 0)
+    # If no parenthesis, it gives back the whole tuple, port included.
+    return name[0]
 
-def scan(ip):
-    arp_request = ARP(pdst=ip)
+
+def scan(ip, mask):
+    arp_request = ARP(pdst=ip+"/"+mask)
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = broadcast / arp_request
     ask_list = srp(packet, timeout=1, verbose=False)[0]
 
     packet_list = []
     for i in ask_list:
-        packet_dict = {"ip": i[1].psrc, "mac": i[1].hwsrc}
+        packet_dict = {"IP Address": i[1].psrc, "MAC Address": i[1].hwsrc}
         packet_list.append(packet_dict)
-    return (packet_list)
-
-print(scan("192.168.178.0/24"))
-#Scan all the addresses (except the lowest, which is your network address and the highest, which is your broadcast address).
-#Use your DNS's reverse lookup to determine the hostname for IP addresses which respond to your scan.
-#sistemare codice
-calculate_network(addr, mask)
-
-# https://stackoverflow.com/questions/207234/list-of-ip-addresses-hostnames-from-local-network-in-python
+    for element in packet_list:
+        ip_host = element["IP Address"]
+        name = hostname(ip_host)
+        element["Hostname"] = name
+    return packet_list
